3c8971ded4a8c55cfc15404e1cc949e40c30e258
Anna Guerrero <acg2176@columbia.edu>
2020-04-25 21:55:01 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -3,8 +3,8 @@ require_relative "../lib/student.rb"
 class ChattyStudent < Student
 
   def hello
-    puts super
-    puts "How are you doing today? I'm okay,
+
+    puts "#{super} + How are you doing today? I'm okay,
     but I'm kind of tired. Did you watch The
     Walking Dead last night? You didn't?! Oh
     man, it was so crazy! What, you don't want
